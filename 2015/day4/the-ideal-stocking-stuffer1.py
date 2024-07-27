f073687180e5954686dba1844e0b2677c6c01c24
Erik Svensson <ersv1001@gmail.com>
2024-07-27 12:55:03 +0200

    day4(2015): part1 & part2

---
+++ 2015/day4/the-ideal-stocking-stuffer1.py
@@ -0,0 +1,21 @@
+import hashlib
+
+md5 = hashlib.md5
+input_file = open("input1.txt", "r")
+input = input_file.read()
+
+counter = 0
+
+
+def check_hash(hash_input):
+    result = md5(hash_input.encode())
+    result_hex = result.hexdigest()
+    if result_hex[:5] == "00000":
+        return True
+    return False
+
+
+while check_hash(input + str(counter)) is False:
+    counter += 1
+
+print(counter)
