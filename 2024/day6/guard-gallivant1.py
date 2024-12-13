bc6ecc942cfae6bb9cd92ba2533c83cf59abc7e8
Erik Svensson <ersv1001@gmail.com>
2024-12-13 17:08:35 +0100

    day6(2024): part1 WIP

--- 2024/day6/guard-gallivant1.py
+++ 2024/day6/guard-gallivant1.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+total = 0
 position = [0, 0]
 direction = "^"
 guard_map = np.empty(shape=(10, 10), dtype=str)
@@ -11,8 +12,37 @@ with open("input_small.txt") as file:
             if column in {"^", ">", "v", "<"}:
                 direction = column
                 position = [x, y]
-            guard_map[x, y] = column
+            guard_map[x, y] = x
 
+# try:
+#    while 1:
+#        if guard_map[position[0], position[1]] != "X":
+#            guard_map[position[0], position[1]] = "X"
+#            total += 1
+#        if direction == '^':
+#            print("here")
+#            if guard_map[position[0], position[1] - 1] == "#":
+#                direction = '>'
+#            else:
+#                position[1] += 1
+#        if direction == '>':
+#            if guard_map[position[0] + 1, position[1]] == "#":
+#                direction = 'v'
+#            else:
+#                position[0] += 1
+#        if direction == 'v':
+#            if guard_map[position[0], position[1] - 1] == "#":
+#                direction = '<'
+#            else:
+#                position[1] -= 1
+#        if direction == '<':
+#            if guard_map[position[0] - 1, position[1]] == "#":
+#                direction = '^'
+#            else:
+#                position[0] -= 1
+# except:
+#    print("Guard left map")
 
 print(guard_map)
-print(position)
+# print(position)
+print(total)
